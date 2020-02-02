1c80c1aa741e6af997f19d35ebd5b54c89fe44cc
JPZCode <jpzcode02@gmail.com>
2020-02-01 19:44:37 -0600

    Vector dinámico y for

---
+++ Vector.java
@@ -0,0 +1,18 @@
+public class Vector {
+    public static void main(String[] args) {
+        
+        int numeros[] = new int[5];
+
+        numeros[0] = 1;
+        numeros[1] = 582;
+        numeros[2] = 45;
+        numeros[3] = 2;
+        numeros[4] = 785;
+
+        System.out.print("[" + numeros[0] + "]");
+        System.out.print("[" + numeros[1] + "]");
+        System.out.print("[" + numeros[2] + "]");
+        System.out.print("[" + numeros[3] + "]");
+        System.out.print("[" + numeros[4] + "]");
+    }
+}
