6d62c57479b04db839ff3fa7adddf1c00622df78
JPZCode <jpzcode02@gmail.com>
2020-04-05 15:41:33 -0500

    Builder ejemplo sencillo terminado

--- Builder/src/main/Main.java
+++ Builder/src/main/Main.java
@@ -1,1 +1,14 @@
 package main;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Usuario codi = Usuario.Make("Pamela", "Pérez")
+								.setEmail("jesypame321@gmail.com")
+								.setTelefono("442-3183-544")
+								.Build();
+		
+		System.out.println(codi);
+	}
+}
--- Builder/src/main/Usuario.java
+++ Builder/src/main/Usuario.java
@@ -2,4 +2,47 @@ package main;
 
 public class Usuario {
 
+	private String nombre;
+	private String apellido;
+	
+	private String email;
+	private String telefono;
+	private String direccion;
+
+	public Usuario setEmail(String email) {
+		this.email = email;
+		return this;
+	}
+
+	public Usuario setTelefono(String telefono) {
+		this.telefono = telefono;
+		return this;
+	}
+
+	public Usuario setDireccion(String direccion) {
+		this.direccion = direccion;
+		return this;
+	}
+		
+	private Usuario(String nombre, String apellido) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		
+		this.email = "";
+		this.telefono = "";
+		this.direccion = "";
+	}
+	
+	public static Usuario Make(String nombre, String apellido) {
+		return new Usuario(nombre, apellido);
+	}
+	
+	public Usuario Build() {
+		return this;
+	}
+	
+	public String toString() {
+		return " " + this.nombre + " " + this.apellido + " " + this.email + " " + this.telefono + " " + this.direccion;
+	}
+	
 }
